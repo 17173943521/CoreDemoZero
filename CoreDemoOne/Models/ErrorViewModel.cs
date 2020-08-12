329c9564c5795b10e03253460a880ef37719dc99
北大还行 <819604303@qq.com>
2020-08-12 09:15:09 +0800

    first

---
+++ CoreDemoOne/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDemoOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
